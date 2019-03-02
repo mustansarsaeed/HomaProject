d20775b85e110556c8cec923c0a85d9fe1c0b0af
Mustansar Saeed <m.mustansar.saeed@gmail.com>
2019-03-02 18:11:26 +0500

    - Retransmission of messages sample tested

--- txc1.cc
+++ txc1.cc
@@ -10,34 +10,81 @@
 
 using namespace omnetpp;
 
-class TxcBase : public cSimpleModule {
-    private:
-        int counter;
-    protected:
-        virtual void initialize() override;
-        virtual void handleMessage(cMessage *cmsg) override;
+class Tic : public cSimpleModule
+{
+  private:
+    simtime_t timeout;
+    cMessage *timeoutEvent;
+
+  public:
+    Tic();
+    virtual ~Tic();
+
+  protected:
+    virtual void initialize() override;
+    virtual void handleMessage(cMessage *msg) override;
 };
 
-Define_Module(TxcBase);
+Define_Module(Tic);
 
-void TxcBase::initialize() {
-    counter = par("limit");
-    WATCH(counter);
-    if(par("sendMsgOnInit").boolValue() == true) {
-        cMessage* cmsg = new cMessage("Hello Toc");
-        EV << "Sending initial message\n";
-        send(cmsg, "out");
+Tic::Tic()
+{
+    timeoutEvent = nullptr;
+}
+
+Tic::~Tic()
+{
+    // Dispose of dynamically allocated the objects
+    cancelAndDelete(timeoutEvent);
+}
+
+void Tic::initialize()
+{
+    timeout = 1.0;
+    timeoutEvent = new cMessage("timeoutEvent");
+    EV << "Sending initial messages\n";
+    cMessage *cmsg = new cMessage ("TicTocMessage");
+    send(cmsg, "out");
+    scheduleAt(simTime()+timeout, timeoutEvent);
+}
+
+void Tic::handleMessage(cMessage *msg)
+{
+    if(msg == timeoutEvent) {
+        EV << "Timeout expired, resending message and restarting timer\n";
+        cMessage *newMsg = new cMessage("Resending new message");
+        send(newMsg, "out");
+        scheduleAt(simTime()+timeout, timeoutEvent);
+    } else {
+        EV << "Timer cancelled.\n";
+       cancelEvent(timeoutEvent);
+       delete msg;
+
+       // Ready to send another one.
+      cMessage *newMsg = new cMessage("Sending new tictocMsg");
+      send(newMsg, "out");
+      scheduleAt(simTime()+timeout, timeoutEvent);
     }
 }
 
-void TxcBase::handleMessage(cMessage* cmsg) {
-    counter--;
-    if(counter == 0) {
-        EV << cmsg->getName() << "'s counter reached zero\n";
-        delete cmsg;
-    } else {
-        EV << getName() << "'s counter is " << counter << ", sending back message\n";
-        send(cmsg, "out");
-    }
+class Toc : public cSimpleModule
+{
+  protected:
+    virtual void handleMessage(cMessage *msg) override;
+};
 
+Define_Module(Toc);
+
+void Toc::handleMessage(cMessage *msg)
+{
+    EV << "uniform(0, 1) = " << uniform(0, 1) ;
+    if (uniform(0, 1) < 0.5) {
+        EV << "\"Losing\" message.\n";
+        bubble("message lost");  // making animation more informative...
+        delete msg;
+    }
+    else {
+        EV << "Sending back same message as acknowledgement.\n";
+        send(msg, "out");
+    }
 }
